5f3f114bb3b366216fcd5127e0bf474ee738ef9a
Sam Milledge <sam@sammilledge.com>
2017-03-04 15:54:54 -0800

    Update Redis feeder to be consistent with new style

--- chomper/contrib/redis.py
+++ chomper/contrib/redis.py
@@ -1,5 +1,8 @@
 from __future__ import absolute_import
 
+import six
+
+from chomper import Item, config
 from chomper.exceptions import NotConfigured
 from chomper.feeders import Feeder
 
@@ -9,7 +12,7 @@ except ImportError:
     raise NotConfigured('Redis library not installed')
 
 
-class RedisFeed(Feeder):
+class RedisFeeder(Feeder):
     """
     Redis item feed
 
@@ -18,19 +21,25 @@ class RedisFeed(Feeder):
 
     timeout = 5
 
-    def __init__(self, key, host='localhost', port=6379, redis_args=None):
+    def __init__(self, key, host=None, port=None, redis_args=None):
         if redis_args is None:
             redis_args = dict()
 
+        if isinstance(key, six.string_types):
+            key = [key]
+
         self.key = key
+
+        host = host if host is not None else config.get('redis', 'host', 'localhost')
+        port = port if port is not None else config.getint('redis', 'port', 6379)
         self.redis = redis.StrictRedis(host=host, port=port, **redis_args)
 
     def feed(self, item):
-        result = self.redis.blpop([self.key], self.timeout)
+        result = self.redis.blpop(self.key, self.timeout)
 
         if result is None:
             # There's nothing left in the queue
             yield None
         else:
             source, data = result
-            yield data
+            yield Item(**data)
